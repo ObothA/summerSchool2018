cd7df6a5a11a7e1d2c910d707c91db71b86433ac
StevenAraka <stevenaraka@gmail.com>
2018-06-11 13:53:25 +0300

    function to multiply

--- Add.js
+++ Add.js
@@ -1,16 +1,18 @@
-
-function add(num1,num2){
+function add(num1, num2) {
     sum = num1 + num2
     return sum
 }
 
-console.log( add(5,7) )
+console.log(add(5, 7))
 
-function subtract(num1,num2){
+function subtract(num1, num2) {
     result = num1 - num2
     return result
 }
 
-console.log(subtract(20,10))
+console.log(subtract(20, 10))
 
-
+function product(var1, var2) {
+    return var1 * var2;
+}
+console.log(product(2, 3));
